0a7b27e59893f7a8d57cdf2b65f682bcac76b847
Carlos Macasaet <github@carlos-m.net>
2022-06-28 20:34:59 -0700

    Tidy fuzz configuration
    This adds missing license notices and removes debug code. It also reduces
    the fuzzing time to 1 minute on pull requests.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-set -e
-set -u
-set -x
+#   Copyright 2022 Carlos Macasaet
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+set -eu
 
 # Build the project to fuzz and extract the version
 mvn --batch-mode \
@@ -30,44 +42,11 @@ RUNTIME_CLASSPATH="\${this_dir}/fernet-java8.jar:\${this_dir}/fernet-fuzzer.jar"
 
 # if there are more fuzzers, turn this into a loop
 
-cp /src/default.options "${OUT}/TokenEncryptDecryptFuzzer.options"
+fuzzer="TokenEncryptDecryptFuzzer"
+cp "${SRC}/default.options" "${OUT}/${fuzzer}.options"
 
-# TODO use m4 template
-# TODO shellcheck generated file
-cat << EOF > "${OUT}/TokenEncryptDecryptFuzzer"
-#!/bin/sh
-
-set -e
-set -x
-
-pwd
-ls -lha
-
-this_dir=\$(dirname "\$0")
-CLASSPATH="$RUNTIME_CLASSPATH"
-
-OIFS="\$IFS"
-IFS=:
-for element in \$CLASSPATH; do
-  test -r \$element
-done
-IFS="\$OIFS"
-test -x "\${this_dir}/jazzer_driver"
-test -r "\${this_dir}/jazzer_agent_deploy.jar"
-
-# Magic comment, do not remove:
-# LLVMFuzzerTestOneInput
-# It's required by ClusterFuzzLine test_all.py
-# https://github.com/google/oss-fuzz/blob/1d588e62cdc119f676316fbcab13cc331c7fb08c/infra/base-images/base-runner/test_all.py#L95
-# It's how the framework identifies that this file is a fuzz target
-
-export LD_LIBRARY_PATH=${JVM_LD_LIBRARY_PATH}
-export ASAN_OPTIONS="\${ASAN_OPTIONS}:symbolize=1:external_symbolizer_path=\${this_dir}/llvm-symbolizer:detect_leaks=0"
-"\${this_dir}/jazzer_driver" \
-  --agent_path="\${this_dir}/jazzer_agent_deploy.jar" \
-  --cp="\${CLASSPATH}" \
-  --target_class=TokenEncryptDecryptFuzzer \
-  --jvm_args="-Xmx2048m:-Djava.awt.headless=true" \
-  \$@
-EOF
-chmod +x "${OUT}/TokenEncryptDecryptFuzzer"
+m4 -D xCLASSPATH="$RUNTIME_CLASSPATH" \
+  -D xLD_LIBRARY_PATH="$JVM_LD_LIBRARY_PATH" \
+  "${SRC}/fuzz_target.m4" > "${OUT}/${fuzzer}"
+shellcheck "${OUT}/${fuzzer}"
+chmod +x "${OUT}/${fuzzer}"
